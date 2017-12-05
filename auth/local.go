fc2614d619e25a9e23e3db7ee214021ed2fe7755
Richard Heath <rheath@humanlongevity.com>
2017-12-04 22:12:20 -0800

    update auth provider

--- auth/local.go
+++ auth/local.go
@@ -1,13 +1,81 @@
 package auth
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
 // LocalProvider Use local JSON file for auth.
 type LocalProvider struct {
+	config authConfig
 }
 
-func (provider LocalProvider) initialize(config map[string]interface{}) error {
+type authConfig struct {
+	Users map[string]userConfig   `json:"users"`
+	Repos map[string]RepoSettings `json:"repos"`
+}
+
+type userConfig struct {
+	Apikey          string            `json:"apikey"`
+	RepoPermissions map[string]string `json:"repoPermissions"`
+}
+
+// Initialize Load auth config JSON file.
+func (provider *LocalProvider) Initialize(config map[string]interface{}) error {
+	rawPath, pathGiven := config["path"]
+	if !pathGiven {
+		return fmt.Errorf("Config path not given")
+	}
+
+	configPath := fmt.Sprintf("%v", rawPath)
+	provider.config = authConfig{}
+	rawConfig, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(rawConfig, &provider.config)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
-func (provider LocalProvider) Authenticate(username, password, repo string) (success bool, err error) {
+// Authenticate Authenticate user.
+func (provider *LocalProvider) Authenticate(username, apikey string) (success bool, err error) {
+	user, userFound := provider.config.Users[username]
+	if !userFound {
+		return false, nil
+	}
+
+	if user.Apikey != apikey {
+		return false, nil
+	}
+
 	return true, nil
+}
+
+func (provider *LocalProvider) GetUserRepoPermission(username, repo string) string {
+	user, userFound := provider.config.Users[username]
+	if !userFound {
+		return ""
+	}
+
+	repoPermission, repoFound := user.RepoPermissions[repo]
+	if !repoFound {
+		return ""
+	}
+
+	return repoPermission
+}
+
+func (provider *LocalProvider) GetRepoSettings(repoName string) (RepoSettings, error) {
+	repo, repoFound := provider.config.Repos[repoName]
+	if !repoFound {
+		return RepoSettings{}, fmt.Errorf("repo not found: " + repoName)
+	}
+
+	return repo, nil
 }
--- auth/provider.go
+++ auth/provider.go
@@ -4,20 +4,27 @@ import "fmt"
 
 // Provider Interface for auth provider.
 type Provider interface {
-	initialize(config map[string]interface{}) error
-	Authenticate(username, password, repo string) (success bool, err error)
+	Initialize(config map[string]interface{}) error
+	Authenticate(username, apikey string) (success bool, err error)
+	GetUserRepoPermission(username, repo string) string
+	GetRepoSettings(repo string) (RepoSettings, error)
+}
+
+type RepoSettings struct {
+	AllowAnonymousRead  bool `json:"allowAnonymousRead"`
+	AllowAnonymousWrite bool `json:"allowAnonymousWrite"`
 }
 
 // InitializeProvider Initialize data provider.
 func InitializeProvider(providerType string, config map[string]interface{}) (Provider, error) {
 	var provider Provider
 	if providerType == "local" {
-		provider = LocalProvider{}
+		provider = &LocalProvider{}
 	} else {
 		return nil, fmt.Errorf("Auth provider not supported: %s", providerType)
 	}
 
-	err := provider.initialize(config)
+	err := provider.Initialize(config)
 	if err != nil {
 		return nil, err
 	}
